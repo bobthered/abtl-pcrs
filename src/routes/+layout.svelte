82f0a27b13dd79d6817d5d64b51021a7cfc7e07b
Bob McAleavey <bobmcaleavey@gmail.com>
2024-07-19 15:34:49 -0400

    feat: add header text color class

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,12 +8,13 @@
 	const navMap = new Map([
 		['/job-classifications', 'Job Classifications'],
 		['/pcrs', 'PCRs'],
+		['/pcr-reasons', 'PCR Reasons'],
 		['/users', 'Users']
 	]);
 </script>
 
 <Div class="flex max-h-screen min-h-screen flex-grow flex-col overflow-auto">
-	<Header class="bg-primary-500">
+	<Header class="bg-primary-500 text-white">
 		<Nav>
 			{#each [...navMap] as [href, label]}
 				<A class="px-6 py-3" {href}>{label}</A>
