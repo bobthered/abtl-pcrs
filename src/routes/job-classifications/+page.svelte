ca6ae9527e6a9b222daa0420a355112699894d0b
Bob McAleavey <bobmcaleavey@gmail.com>
2024-07-19 15:36:05 -0400

    feat: update job-classifications route to use dataTable server functions

--- src/routes/job-classifications/+page.svelte
+++ src/routes/job-classifications/+page.svelte
@@ -3,19 +3,10 @@
 
 	// props
 	let { data } = $props();
-	let columnFilterKeys = $state(['id', 'facility', 'userIds']);
-	let columnTypes: Map<string, 'int' | 'text'> = $state(
-		new Map([
-			['description', 'text'],
-			['costCenter', 'int'],
-			['code', 'int'],
-			['eeoClassification', 'int'],
-			['title', 'text'],
-			['workCompClass', 'int']
-		])
-	);
+	let columnsToHide = $state(['id', 'facility', 'userIds', 'users']);
+	let sortKey = $state('description');
 </script>
 
 <Div class="flex flex-grow flex-col overflow-auto p-4">
-	<DataTable bind:columnFilterKeys bind:columnTypes bind:rows={data.jobClassifications} />
+	<DataTable bind:columnsToHide bind:fields={data.fields} bind:rows={data.rows} bind:sortKey />
 </Div>
