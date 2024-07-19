7e28899d40cdf104e02fef5e835146745ae6b7f8
Bob McAleavey <bobmcaleavey@gmail.com>
2024-07-19 15:36:15 -0400

    feat: update users route to use dataTable server functions

--- src/routes/users/+page.server.ts
+++ src/routes/users/+page.server.ts
@@ -1,65 +1,21 @@
-import format from '$lib/format';
-import { prisma } from '$lib/prisma';
-import type { Actions } from '@sveltejs/kit';
+import bcrypt from 'bcryptjs';
+import { DEFAULT_USER_PASSWORD } from '$env/static/private';
+import { server } from '$lib/dataTable';
 
-const _getData = async () => {
-	const data = await Promise.all([
-		prisma.jobClassification.findMany(),
-		prisma.user.findMany(),
-		prisma.userProfile.findMany()
-	]);
-	return data;
-};
-export const actions: Actions = {
-	create: async () => {
-		const rows = await prisma.user.findMany();
-		await prisma.user.create({
-			data: {
-				username: `* user${rows.length}`,
-				isActive: false,
-				isOnboarded: false,
-				passwordHash: ''
-			}
+const { actions, load } = await server('User', {
+	beforeCreate: async (data) => {
+		const passwordHash: string = await new Promise((res) => {
+			bcrypt.hash(DEFAULT_USER_PASSWORD, 10, (err, hash) => {
+				return res(hash);
+			});
 		});
-
-		const [jobClassifications, users, userProfiles] = await _getData();
-
-		return { jobClassifications, success: true, users, userProfiles };
+		data.passwordHash = passwordHash;
+		return data;
 	},
-	delete: async ({ request }) => {
-		const { id } = format.formData.to.object(await request.formData());
-
-		await prisma.user.delete({ where: { id } });
-
-		const [jobClassifications, users, userProfiles] = await _getData();
-
-		return { jobClassifications, success: true, users, userProfiles };
-	},
-	upsert: async ({ request }) => {
-		const { id, ...data } = format.formData.to.object(await request.formData());
-		const update = {
-			isActive: data.isActive === 'true',
-			isOnboarded: data.isOnboarded === 'true',
-			jobClassificationId:
-				data.jobClassificationId === 'null' ? undefined : data.jobClassificationId,
-			passwordHash: data.passwordHash,
-			profileId: data.profileId === 'null' ? undefined : data.profileId,
-			username: data.username
-		};
-		await prisma.user.upsert({
-			where: {
-				id
-			},
-			update,
-			create: update
-		});
-
-		return { success: true };
+	relations: {
+		jobClassificationId: { modelName: 'JobClassification', label: 'description' },
+		profileId: { modelName: 'UserProfile', label: (row) => `${row.firstName} ${row.lastName}` }
 	}
-};
+});
 
-export const load = async () => {
-	const [jobClassifications, users, userProfiles] = await _getData();
-
-	return { jobClassifications, userProfiles, users };
-};
+export { actions, load };
--- src/routes/users/+page.svelte
+++ src/routes/users/+page.svelte
@@ -1,43 +1,25 @@
 <script lang="ts">
-	import { DataTable, Div, Select, Td } from '$lib/components';
+	import { DataTable, Div } from '$lib/components';
 
 	// props
 	let { data } = $props();
-	let columnFilterKeys = $state(['id', 'passwordHash']);
-	let columnTypes = $state(
-		new Map([
-			['isActive', 'boolean'],
-			['isOnboarded', 'boolean'],
-			[
-				'jobClassificationId',
-				{
-					component: Select,
-					props: {
-						options: data.jobClassifications
-							.map(({ id, description }) => ({ label: description, value: id }))
-							.sort((a: any, b: any) => a.label.localeCompare(b.label))
-					}
-				}
-			],
-			[
-				'profileId',
-				{
-					component: Select,
-					props: {
-						options: data.userProfiles
-							.map(({ id, firstName, lastName }) => ({
-								label: `${firstName} ${lastName}`,
-								value: id
-							}))
-							.sort((a: any, b: any) => a.label.localeCompare(b.label))
-					}
-				}
-			]
-		])
-	);
-	let sortKey = $state('username');
+	let columnsToHide = $state([
+		'id',
+		'jobClassification',
+		'passwordHash',
+		'pcrIds',
+		'pcrs',
+		'profile'
+	]);
+	let sortKey = $state('profileId');
 </script>
 
 <Div class="flex flex-grow flex-col overflow-auto p-4">
-	<DataTable bind:columnFilterKeys bind:columnTypes bind:rows={data.users} bind:sortKey></DataTable>
+	<DataTable
+		bind:columnsToHide
+		bind:fields={data.fields}
+		bind:relationOptions={data.relationOptions}
+		bind:rows={data.rows}
+		bind:sortKey
+	/>
 </Div>
